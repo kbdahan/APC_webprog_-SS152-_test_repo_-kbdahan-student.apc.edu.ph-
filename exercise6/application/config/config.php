bbe5a432e2a7bd771d8e81e343ea5cb2097bc590
Nikko Dahan <ftwzombeh101@gmail.com>
2016-12-20 19:10:11 +0800

    s

--- exercise6/application/config/config.php
+++ exercise6/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost:8080/exercise6/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'home.php';
+$config['index_page'] = 'index_home.php';
 
 /*
 |--------------------------------------------------------------------------
