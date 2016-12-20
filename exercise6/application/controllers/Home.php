d14affcfc0975bb1f42f01306dad581e393c7937
Nikko Dahan <ftwzombeh101@gmail.com>
2016-12-21 00:14:50 +0800

    s

--- exercise6/application/controllers/Home.php
+++ exercise6/application/controllers/Home.php
@@ -3,10 +3,12 @@
 class Home extends CI_Controller {
 	public function index()
 	{
+	$this->load->view('Head');
 	$this->load->view('Home');
 	}
 	public function about()
 	{
+	$this->load->view('Head');
 	$this->load->view('about');
 	}
 	public function PhotoGallery()
