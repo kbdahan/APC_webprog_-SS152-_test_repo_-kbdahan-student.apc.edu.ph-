31c9b4ce5ad39dbbbc7e20e10b6779903da38786
Nikko Dahan <ftwzombeh101@gmail.com>
2016-12-21 11:50:58 +0800

    modify

--- exercise6/application/views/highschoolgallery.php
+++ exercise6/application/views/highschoolgallery.php
@@ -1,35 +1,5 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Highschool Gallery</title>
-<script type="text/javascript">
-function MM_preloadImages() { //v3.0
-  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
 
-function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
-
-function MM_findObj(n, d) { //v4.01
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && d.getElementById) x=d.getElementById(n); return x;
-}
-
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
-}
-</script>
-</head>
-
-<body onLoad="MM_preloadImages('img/highschool/1795690_356636904476862_1579494759_n.jpg','img/highschool/1234196_356639067809979_2054459325_n.jpg','img/highschool/1911955_356641151143104_602857658_n.jpg','img/highschool/1926941_356640314476521_1950469413_n.jpg','img/highschool/1970653_356639771143242_2001329325_n.jpg','img/highschool/10013505_361009544039598_195923794_n.jpg')">
+<body onLoad="MM_preloadImages('<?php echo base_url('assets/uploads/img/highschool/1795690_356636904476862_1579494759_n.jpg');?>','<?php echo base_url('assets/uploads/img/highschool/1234196_356639067809979_2054459325_n.jpg');?>','<?php echo base_url('assets/uploads/img/highschool/1911955_356641151143104_602857658_n.jpg');?>','<?php echo base_url('assets/uploads/img/highschool/1926941_356640314476521_1950469413_n.jpg');?>','<?php echo base_url('assets/uploads/img/highschool/1970653_356639771143242_2001329325_n.jpg');?>','<?php echo base_url('assets/uploads/img/highschool/10013505_361009544039598_195923794_n.jpg');?>)">
 <center>
 <table style = "background-color:black; "width="610" border="0" cellpadding="2">
 <caption>
@@ -37,15 +7,14 @@ function MM_swapImage() { //v3.0
   </caption>
   <tbody>
     <tr>
-      <th colspan="6" bgcolor="#212121" scope="col"><img src="img/highschool/1795690_356636904476862_1579494759_n.jpg" alt="" width="863" height="621" id="main"/></th>
+      <th colspan="6" bgcolor="#212121" scope="col"><img src="<?php echo base_url('assets/uploads/img/highschool/1795690_356636904476862_1579494759_n.jpg');?>" alt="" width="863" height="621" id="main"/></th>
     </tr>
-    <tr>
-      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','img/highschool/1795690_356636904476862_1579494759_n.jpg',1)"><img src="img/highschool/1795690_356636904476862_1579494759_n (Copy).jpg" alt="" width="197" height="176" id="mainthumb" border="0"/></a></td>
-      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','img/highschool/1234196_356639067809979_2054459325_n.jpg',1)"><img src="img/highschool/1234196_356639067809979_2054459325_n (Copy).jpg" alt="" width="197" height="177" id="thumb2" border="0"/></a></td>
-      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','img/highschool/1911955_356641151143104_602857658_n.jpg',1)"><img src="img/highschool/1911955_356641151143104_602857658_n (Copy).jpg" alt="" width="210" height="188" id="thumb3" border="0"/></a></td>
-      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','img/highschool/1926941_356640314476521_1950469413_n.jpg',1)"><img src="img/highschool/1926941_356640314476521_1950469413_n (Copy).jpg" alt="" width="224" height="181" id="thumb4" border="0"/></a></td>
-      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','img/highschool/1970653_356639771143242_2001329325_n.jpg',1)"><img src="img/highschool/1970653_356639771143242_2001329325_n (Copy).jpg" alt="" width="212" height="167" id="thumb5" border="0"/></a></td>
-      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','img/highschool/10013505_361009544039598_195923794_n.jpg',1)"><img src="img/highschool/10013505_361009544039598_195923794_n (Copy).jpg" alt="" width="190" height="188" id="thumb6" border="0"/></a></td>
+    <tr><?php echo base_url('assets/uploads/
+      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','<?php echo base_url('assets/uploads/img/highschool/1795690_356636904476862_1579494759_n.jpg');?>',1)"><img src="<?php echo base_url('assets/uploads/img/highschool/1795690_356636904476862_1579494759_n (Copy).jpg');?>" alt="" width="197" height="176" id="mainthumb" border="0"/></a></td>
+      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','<?php echo base_url('assets/uploads/img/highschool/1234196_356639067809979_2054459325_n.jpg');?>',1)"><img src="<?php echo base_url('assets/uploads/img/highschool/1234196_356639067809979_2054459325_n (Copy).jpg');?>" alt="" width="197" height="177" id="thumb2" border="0"/></a></td>
+      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','<?php echo base_url('assets/uploads/img/highschool/1911955_356641151143104_602857658_n.jpg');?>',1)"><img src="<?php echo base_url('assets/uploads/img/highschool/1911955_356641151143104_602857658_n (Copy).jpg');?>" alt="" width="210" height="188" id="thumb3" border="0"/></a></td>
+      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','<?php echo base_<?php echo base_url('assets/uploads/img/highschool/1970653_356639771143242_2001329325_n.jpg');?>',1)"><img src="<?php echo base_url('assets/uploads/img/highschool/1970653_356639771143242_2001329325_n (Copy).jpg');?>" alt="" width="212" height="167" id="thumb5" border="0"/></a></td>
+      <td bgcolor="#000000"><a href="javascript:;" onClick="MM_swapImage('main','','<?php echo base_url('assets/uploads/img/highschool/10013505_361009544039598_195923794_n.jpg');?>',1)"><img src="<?php echo base_url('assets/uploads/img/highschool/10013505_361009544039598_195923794_n (Copy).jpg');?>" alt="" width="190" height="188" id="thumb6" border="0"/></a></td>
     </tr>
   </tbody>
 </table>
